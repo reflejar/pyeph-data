48a987c3d388d2cccc83812f83d4be550c9d32cf
Pano Valdez <marianovaldez92@protonmail.com>
2024-11-27 11:28:25 -0300

    finalizo descarga

--- getter.py
+++ getter.py
@@ -3,6 +3,7 @@ import re
 import os
 import requests
 import zipfile
+import shutil
 import pandas as pd
 
 # Expresión regular para validar una URL
@@ -22,26 +23,26 @@ def descargar_y_procesar(url):
     trimestre = zip_filename.split('_')[2]
     anio = zip_filename.split('_')[4]
     # Crear carpeta ./tmp si no existe
-    # os.makedirs("./tmp", exist_ok=True)
+    os.makedirs("./tmp", exist_ok=True)
 
-    # # Descargar el archivo ZIP
-    # response = requests.get(url)
-    # if response.status_code == 200:
-    #     with open(zip_filename, "wb") as file:
-    #         file.write(response.content)
-    #     print(f"Archivo descargado: {zip_filename}")
-    # else:
-    #     print(f"Error al descargar el archivo: {response.status_code}")
-    #     return
+    # Descargar el archivo ZIP
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(zip_filename, "wb") as file:
+            file.write(response.content)
+        print(f"Archivo descargado: {zip_filename}")
+    else:
+        print(f"Error al descargar el archivo: {response.status_code}")
+        return
 
-    # # Extraer el ZIP
-    # with zipfile.ZipFile(zip_filename, "r") as zip_ref:
-    #     zip_ref.extractall('tmp')
-    # print(f"Archivos extraídos en la carpeta 'tmp'")
+    # Extraer el ZIP
+    with zipfile.ZipFile(zip_filename, "r") as zip_ref:
+        zip_ref.extractall('tmp')
+    print(f"Archivos extraídos en la carpeta 'tmp'")
 
     # Selecciona la primera carpeta 
     folders = [folder for folder in os.listdir("tmp") if os.path.isdir(os.path.join('tmp', folder))]
-    folder = folders[0]
+    folder = os.path.join('tmp', folders[0])
     archivos_txt = [file for file in os.listdir(folder) if file.endswith(".txt")]
     for txt_file in archivos_txt:
         txt_path = os.path.join(folder, txt_file)
@@ -60,21 +61,17 @@ def descargar_y_procesar(url):
 
             # Comprimir el CSV en un ZIP
             with zipfile.ZipFile(f'{base_name}/{zip_output}', "w", zipfile.ZIP_DEFLATED) as zip_out:
-                zip_out.write(f'./tmp/{csv_output}')
+                zip_out.write(f'./tmp/{csv_output}', arcname=csv_output)
             print(f"Archivo ZIP generado: {zip_output}")
 
         else:
             print(f"Archivo {txt_file} no encontrado en el ZIP.")
 
-    # # Limpiar la carpeta temporal
-    # for root, dirs, files in os.walk("tmp"):
-    #     for file in files:
-    #         os.remove(os.path.join(root, file))
-    # os.rmdir("tmp")
-
-    # # Eliminar el ZIP original descargado
-    # os.remove(zip_filename)
-    # print("Proceso completado.")
+    # Limpiar la carpeta temporal
+    try:
+        shutil.rmtree("./tmp")
+    except:
+        print("No se pudo eliminar carpeta tmp")
 
 
 if len(sys.argv) > 1:
